e63b6e1f91369c21d67188d11d7a2e22e2025567
zjackz <jackzhang.public@gmail.com>
2013-02-01 16:04:59 +0800

    v1
    start from here

---
+++ Demo4CSharp/Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Consoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("v1");
+            Console.ReadLine();
+        }
+    }
+}
